a93fa7622a1b0e45a7d876c03b436004494b711e
Алексей Лагунов <sapp1507@gmail.com>
2022-07-25 20:10:54 +0300

    fix

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -16,8 +16,8 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='db'),
-        'USER': os.getenv('DB_USER', default='user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', default='password'),
+        'USER': os.getenv('POSTGRES_USER', default='user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='password'),
         'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
         'PORT': os.getenv('DB_PORT', default=5432),
     }
