f7e4b06c6bda9220db02ea96650c787c824241df
Алексей Лагунов <sapp1507@gmail.com>
2022-07-26 10:55:40 +0300

    fix

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -21,7 +21,8 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientsInLine]
     readonly_fields = ['favorite_count']
     # filter_horizontal = ['tags']
-    fields = ['author', 'name', 'image', 'text', 'cooking_time', 'tags',
+    fields = ['pub_date', 'author', 'name', 'image', 'text', 'cooking_time',
+              'tags',
               'favorite_count', 'favorite', 'shopping_carts']
 
 
